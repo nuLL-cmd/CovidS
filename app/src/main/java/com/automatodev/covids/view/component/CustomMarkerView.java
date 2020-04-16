77b3912ea98614198e65dde82c4f9e785056d845
nuLL-cmd <programadormoderador@hotmail.com>
2020-04-15 22:09:44 -0300

    Adição da visualização dos dados do eixo X no marcador

--- app/src/main/java/com/automatodev/covids/view/component/CustomMarkerView.java
+++ app/src/main/java/com/automatodev/covids/view/component/CustomMarkerView.java
@@ -3,19 +3,19 @@ import android.app.Activity;
 import android.widget.TextView;
 
 import com.automatodev.covids.R;
+import com.automatodev.covids.view.FormatUtils;
 import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.utils.MPPointF;
 
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 public class CustomMarkerView extends MarkerView {
 
     private TextView tvContent;
     private TextView tvContent2;
     private List<String> mxData;
+    private FormatUtils utils;
 
 
     public CustomMarkerView(Activity context, int layoutResource, List<String> mxData) {
@@ -24,6 +24,7 @@ public class CustomMarkerView extends MarkerView {
         tvContent = findViewById(R.id.tvContent);
         tvContent2 = findViewById(R.id.tvContent2);
         this.mxData = mxData;
+        utils = new FormatUtils();
 
     }
 
@@ -31,9 +32,7 @@ public class CustomMarkerView extends MarkerView {
     // content (user-interface)
     @Override
     public void refreshContent(Entry e, Highlight highlight) {
-        Locale locale = new Locale("pt","BR");
-        NumberFormat format = NumberFormat.getInstance(locale);
-            tvContent.setText("Fatais: "+format.format(e.getY()));
+            tvContent.setText("Fatais: "+utils.decimal(e.getY()));
             tvContent2.setText(""+mxData.get((int)e.getX()));
         super.refreshContent(e, highlight);
     }
