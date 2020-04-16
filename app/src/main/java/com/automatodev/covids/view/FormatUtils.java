b0bd4e350bc606f04b3000d5bd2c95e1b6750fc4
nuLL-cmd <programadormoderador@hotmail.com>
2020-04-16 20:12:02 -0300

    Adicionado o metodo apra formatação de data com base em um tipo long

--- app/src/main/java/com/automatodev/covids/view/FormatUtils.java
+++ app/src/main/java/com/automatodev/covids/view/FormatUtils.java
@@ -1,5 +1,6 @@
 package com.automatodev.covids.view;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 public class FormatUtils {
 
@@ -13,6 +14,13 @@ public class FormatUtils {
     public String decimal(float value){
         Locale locale = new Locale("pt","br");
         NumberFormat f = NumberFormat.getInstance(locale);
+
+        return f.format(value);
+    }
+
+    public String dateFormat(long value){
+        Locale locale = new Locale("pt","BR");
+        SimpleDateFormat f = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss",locale);
 
         return f.format(value);
     }
